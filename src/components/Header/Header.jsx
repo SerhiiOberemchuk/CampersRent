f06eaff0d554b74b17f5f2eb34fa7bdc4f12a362
SerhiiOberemchuk <gardenitparts@gmail.com>
2024-05-01 15:26:23 +0200

    home page

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <NavLink />
+        <NavLink />
+        <NavLink />
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
