bc557d18d19c38d95937d7a923145e6a0749ae04
SerhiiOberemchuk <gardenitparts@gmail.com>
2024-05-04 15:08:42 +0200

    form

--- src/components/ModallMoreInfo/BookingCamper/BookingCamper.jsx
+++ src/components/ModallMoreInfo/BookingCamper/BookingCamper.jsx
@@ -7,10 +7,57 @@ const BookingCamper = () => {
       <p className={css.bookingCamperDescription}>
         Stay connected! We are always ready to help you.
       </p>
-      <form action=""></form>
-      <button type="submit" className={css.buttonSubscribe}>
-        Send
-      </button>
+      <form action="subscribe">
+        <div className={css.formGroup}>
+          <label htmlFor="name" className={css.label}></label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.formGroup}>
+          <label htmlFor="email" className={css.label}></label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.formGroup}>
+          <label htmlFor="data" className={css.label}></label>
+          <input
+            type="date"
+            name="data"
+            id="data"
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.formGroup}>
+          <label htmlFor="comment" className={css.label}></label>
+          <input
+            type="text"
+            name="comment"
+            id="comment"
+            placeholder="Comment"
+            className={css.input}
+          />
+        </div>
+
+        <button type="submit" className={css.buttonSubscribe}>
+          Send
+        </button>
+      </form>
     </div>
   );
 };
