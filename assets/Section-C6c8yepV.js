337d23423b033940cef8a55cac343cde45cce772
SerhiiOberemchuk <SerhiiOberemchuk@users.noreply.github.com>
2024-05-19 17:54:01 +0000

    deploy: 36c1ca0981c5796f298f9218957f74f8d6a9abf7

---
+++ assets/Section-C6c8yepV.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CQL7_8E1.js";const s="_pageSection_jxu6f_1",c={pageSection:s},a=({children:e,style:t})=>o.jsx("section",{className:`${c.pageSection} ${t||""}`,children:e});export{a as S};
