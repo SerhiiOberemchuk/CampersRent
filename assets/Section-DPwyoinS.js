ed1b77f47527ddbe4116d299882f11a729435f4e
SerhiiOberemchuk <SerhiiOberemchuk@users.noreply.github.com>
2024-05-19 20:39:56 +0000

    deploy: 12bca26919307c942a4de84950cd6c7caf0623d9

---
+++ assets/Section-DPwyoinS.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-ClNp7SkI.js";const s="_pageSection_jxu6f_1",c={pageSection:s},a=({children:e,style:t})=>o.jsx("section",{className:`${c.pageSection} ${t||""}`,children:e});export{a as S};
