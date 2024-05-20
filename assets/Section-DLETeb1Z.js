45c3ec6cb4b405cf441a598ce90f9f75f9c5eb2e
SerhiiOberemchuk <SerhiiOberemchuk@users.noreply.github.com>
2024-05-20 20:05:24 +0000

    deploy: 34121056733da3619cf7e5c073d5f31f30ad3f4f

---
+++ assets/Section-DLETeb1Z.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-B7g0I7If.js";const s="_pageSection_jxu6f_1",c={pageSection:s},a=({children:e,style:t})=>o.jsx("section",{className:`${c.pageSection} ${t||""}`,children:e});export{a as S};
